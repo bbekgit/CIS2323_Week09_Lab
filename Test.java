87c933c0eb69c73e0fdc96286f26ae73ea6764a4
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2018-10-17 19:27:07 -0500

    Initial commit

---
+++ Test.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Test{
+	public static void main(String []args){
+		String filename = "test.txt";
+		
+		try{
+		FileReader fileReader = new FileReader(fileName);
+		
+		BufferReader bufferReader = new BufferReader(fileReader);
+		
+		while((line = bufferReader.readLine()) ! = null){
+			System.out.println(line);
+		}
+		bufferReader.close();
+		
+		}catch (FileNotFoundException ex){
+			System.out.println("Can't open file: " + fileName);
+		}catch (IOException ex){
+            System.out.println("Error opening file: " + fileName);  			
+		}
+	}
+	
+}
